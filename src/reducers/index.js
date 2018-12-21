f3be4ea8250aab28ab472fd52862a3f1db4b0060
sandykar98k <hello@isandeepbansal.com>
2018-12-21 12:00:42 +0530

    Reducers created

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+  return [
+    { title: 'Start Bucks', duration: '4:05' },
+    { title: 'Not Afraid', duration: '4:30' },
+    { title: 'All Start', duration: '5:00' },
+    { title: 'I Want It That Way', duration: '3:20' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+};
